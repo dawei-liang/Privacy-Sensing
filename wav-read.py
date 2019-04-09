411feb5196b5fd31166c6763ea411179e8facacb
Dawei Liang (David) <32776482+dawei-liang@users.noreply.github.com>
2019-04-09 21:41:05 +0000

    Add files via upload

--- wav-read.py
+++ wav-read.py
@@ -10,11 +10,14 @@ from scipy.io import wavfile
 import os
 import numpy as np
 import resampy
+import copy as cp
 
 import matplotlib.pyplot as plt
 from scipy import signal
 
 import pandas as pd
+
+np.random.seed(0)
 
 #%%
 #os.chdir('/home/dawei/research4')   # Set main dir
@@ -31,6 +34,7 @@ def read_audio_data(file):
 
 def write_audio_data(filename, rate, wav_data):
     wav_data = wav_data * 32768.0   # 16bit
+    wav_data = wav_data.astype(np.int16)
     wavfile.write(filename, rate, wav_data)
     print('Saved')
 
@@ -65,7 +69,7 @@ def frame(data, window_length, hop_length):
 
 def reconstruct_time_series(frames, hop_length_samples):
     '''Reconstruct frames back to the original signals
-    frames.shape = # of frames * window size
+    frames = [# of frames/windows, samples within the window]
     hop_length_samples = # of samples skipped during framing
     '''
     new_signal = []
@@ -100,11 +104,42 @@ def periodic_hann(window_length):
   """
   return 0.5 - (0.5 * np.cos(2 * np.pi / window_length *
                              np.arange(window_length)))
+  
+#%%
+
+def frames_processing(frames, option):
+    '''0 horizontal flip within each frame, 
+    1 vertial flip within each frame, 
+    2 resampling, 
+    3 interpolation and replacement,
+    4 randomly flip frame orders'''
+    
+    if option == 0:
+        frames_new = np.flip(frames,axis=1)
+    elif option == 1:  
+        frames_new = - frames
+    elif option == 2:        
+        frames_new = resampy.resample(frames, 16000, 8000)   # original sr, new sr
+    elif option == 3:
+        frames_new = cp.deepcopy(frames)
+        percentage = 0.5   # % to be replaced
+        idx = np.random.randint(low=50, high=frames.shape[0]-50, 
+                                size=(round(percentage*frames.shape[0]), 1))
+        for i in idx:
+            neighbour = np.random.randint(low=i-50, high=i+50)
+            frames_new[i] = frames[neighbour]   # replaced by neighbourhood
+        print('new frames shape:', frames_new.shape)
+    elif option ==4:
+        frames_new = cp.deepcopy(frames)
+        np.random.shuffle(frames_new)   # only 1st axis is shuffled
+
+    return frames_new
 
 #%%
     
-file = 'G://Research1/codes/audioset/test_data/summer_2018_freesound/scripted study/zhao/0/bathing.wav'
+file = './bathing.wav'
 rate, scaled_data = read_audio_data(file)
+option = 3   # process methods
 
 # Convert to mono.
 if scaled_data.shape[1] > 1:
@@ -121,37 +156,47 @@ window_length_samples = int(round(audio_sample_rate * window_length_secs))
 hop_length_samples = int(round(audio_sample_rate * hop_length_secs))
 fft_length = 2 ** int(np.ceil(np.log(window_length_samples) / np.log(2.0)))
   
-frames = frame(data, hop_length=hop_length_samples, window_length=window_length_samples) 
-#frames_flip = np.flip(frames,axis=1)  # horizontal flip
-frames_flip = - frames   # vertical flip
-
+frames = frame(data, hop_length=hop_length_samples, window_length=window_length_samples)
+frames_new = frames_processing(frames, option = option)
+# create window func
 window = periodic_hann(window_length_samples)
 windowed_frames = frames * window
-windowed_frames_flipped = frames_flip * window
-
+windowed_frames_new = frames_new * window
+# calculate spectrogram
 spectrogram = np.abs(np.fft.rfft(windowed_frames, int(fft_length)))
-spectrogram_flipped = np.abs(np.fft.rfft(windowed_frames_flipped, int(fft_length)))
+spectrogram_new = np.abs(np.fft.rfft(windowed_frames_new, int(fft_length)))
 
 #%%
 plt.figure()
-plt.subplot()
-plt.plot(frames[0])
-plt.xlabel('Time [Sec]')
-plt.show(block = True)
-plt.subplot()
-plt.plot(frames_flip[0])
-plt.xlabel('Time [Sec]')
+plt.plot(frames[1000], label='1')
+plt.plot(frames_new[1000], label='4')
+plt.xlabel('Time [62.5 uSec]')
+plt.legend(loc='upper left')
 plt.show(block = True)
 
 plt.figure()
-plt.pcolormesh(spectrogram.T[:,0:1000])
-plt.ylabel('FFT coe')
-plt.xlabel('Frames')
+plt.pcolormesh(spectrogram.T[:,0:50])
+plt.ylabel('FFT')
+plt.xlabel('0:50 Frames')
+plt.show()
+
+plt.figure()
+plt.pcolormesh(spectrogram_new.T[:,0:50])
+plt.ylabel('FFT')
+plt.xlabel('0:50 Frames')
 plt.show()
 
 #%%
 
-new_signal = reconstruct_time_series(frames, hop_length_samples)
-filename1, filename2, rate, wav_data='./speech.wav', './flipped_speech.wav', 16000, new_signal
-write_audio_data(filename1, rate, data)
-write_audio_data(filename2, rate, wav_data)
+processed_audio_signal = reconstruct_time_series(frames_new, hop_length_samples)
+filename1, filename2, rate, wav_data='./original.wav', './processed.wav', 16000, processed_audio_signal
+write_audio_data(filename1, rate, data)   # save raw signals
+write_audio_data(filename2, rate, processed_audio_signal)   # save processed signals
+
+#%%
+'''temp, testing inverse FFT'''
+spectrogram2 = np.fft.rfft([1,0,0,0,1])
+spectrogram2_flip = np.flip(spectrogram2, axis=0)
+#spectrogram2_flip = -spectrogram2
+windowed_frames2 = np.fft.irfft(spectrogram2_flip, 5)
+print(windowed_frames2)
